d1675175165e6d2b1ac4980a7320ed8b3060fad7
Martin Heide <martin.heide@gmx.net>
2024-06-07 22:03:06 +0200

    Nicer output

--- pre-commit-hook.sh
+++ pre-commit-hook.sh
@@ -5,16 +5,16 @@ if [ "$1" = "--install" ]; then
     this_script="$( realpath "$0" )"
     cd "$( dirname "$0" )"
     mkdir -pv .git/hooks
-    mv .git/hooks/pre-commit .git/hooks/pre-commit.OLD || true
+    rm -fv .git/hooks/pre-commit
     ln -sv "$this_script" .git/hooks/pre-commit
     exit
 fi
 
 readarray -t staged_files < <(git diff --name-only --cached)
-echo "Staged files:" "${staged_files[@]}"
+echo "Linting staged files:" "${staged_files[@]}"
 
-set -x
 if ! npx eslint --no-warn-ignored -- "${staged_files[@]}"; then
     npx eslint --no-warn-ignored --fix -- "${staged_files[@]}"
+    echo "Please check the changes from \"eslint --fix\" and try again." 
     exit 1
 fi
